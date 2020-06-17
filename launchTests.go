42941d600b35802d32c3635d99a5ebc60848abb1
david <s255359@studenti.polito.it>
2020-06-17 16:52:32 +0200

    add Pod to Pod test with Iperf3

--- launchTests.go
+++ launchTests.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	iperfPTP "testk8s/iperf"
+)
 
 func main() {
 	fmt.Println("File da cui lanciare tutto")
+	fmt.Println("Pod to Pod test with Iperf3: ")
+	fmt.Printf("Test 1: %s\n", iperfPTP.IperfPodtoPod())
 }
